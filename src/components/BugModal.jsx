c206a1c7414928fb3cd3b1fb03a3aeae9b8a6f00
bismarkboateng <bismarkb609@gmail.com>
2023-06-06 21:18:19 +0000

    bug component logic state

--- src/components/BugModal.jsx
+++ src/components/BugModal.jsx
@@ -1,4 +1,5 @@
 import { createPortal } from "react-dom";
+import { useState } from "react";
 
 const inputClass = "px-5 py-2 outline-none border-2 border-blue-500 rounded mt-3"
 const labelClass = "font-semibold mt-5 text-white"
@@ -6,11 +7,19 @@ const labelClass = "font-semibold mt-5 text-white"
 
 
 const BugModal = ({ setModal }) => {
+    const [bugInfo, setBugInfo] = useState({
+        ProjectName: "",
+        Description: "",
+        DevId: null
+    })
+
+
 
     function handleSubmitBugInfo(event) {
         setModal(prevState => !prevState)
         event.preventDefault();
         console.log("Save button clicked")
+        console.log(bugInfo)
     }
 
     return createPortal(
@@ -22,19 +31,25 @@ const BugModal = ({ setModal }) => {
                 <input
                     className={inputClass}
                     placeholder="Project Name"
-                    onChange={() => {}}
+                    onChange={(event) => {
+                        setBugInfo((prevState) => ({...prevState, ProjectName: event.target.value}))
+                    }}
                 />
                 <label className={labelClass} >Description: </label> <br />
                 <input
                     className={inputClass}
                     placeholder="Description"
-                    onChange={() => {}}
+                    onChange={(event) => {
+                        setBugInfo((prevState) => ({...prevState, Description: event.target.value}))
+                    }}
                 /> <br />
                 <label className={labelClass} >Dev Id: </label> <br />
                 <input
                     className={inputClass}
                     placeholder="Dev Id"
-                    onChange={() => {}}
+                    onChange={(event) => {
+                        setBugInfo((prevState) => ({...prevState, DevId: Number(event.target.value)}))
+                    }}
                 />
                 <button className="outline-none border-none text-white font-semibold px-10 py-2 rounded bg-blue-500
                     mt-3"
